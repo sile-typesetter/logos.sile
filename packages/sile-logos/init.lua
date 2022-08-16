9818b675a9be2963f1e3e89616b8dd84889f12d1
Caleb Maclennan <caleb@alerque.com>
2022-08-16 15:44:55 +0300

    Convert package to SILE v0.13+ format

---
+++ packages/sile-logos/init.lua
@@ -0,0 +1,19 @@
+local base = require("packages.base")
+
+local package = pl.class(base)
+package._name = "sile-logos"
+
+function package:_init ()
+	base._init(self)
+end
+
+function package:registerCommands ()
+	base.registerCommands(self)
+	-- The first iteration of this package was a collection of commands
+	-- registered using \define{} macros in the SIL input format. They should
+	-- probably be refactored using Lua, but to get the party started with SILE
+	-- v0.13 package support we'll just process them here.
+	SILE.processFile("packages/sile-logos/macros.sil")
+end
+
+return package
