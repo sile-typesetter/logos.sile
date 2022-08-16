3fd8eae771cd578c131be7e690b60709d8c9eb23
Caleb Maclennan <caleb@alerque.com>
2022-08-16 17:26:45 +0300

    Use new API to get package basedir

--- packages/sile-logos/init.lua
+++ packages/sile-logos/init.lua
@@ -3,22 +3,16 @@ local base = require("packages.base")
 local package = pl.class(base)
 package._name = "sile-logos"
 
-local function script_path ()
-   local str = debug.getinfo(2, "S").source:sub(2)
-   return str:match("(.*/)")
-end
-
 function package:_init ()
 	base._init(self)
 end
 
 function package:registerCommands ()
-	base.registerCommands(self)
 	-- The first iteration of this package was a collection of commands
 	-- registered using \define{} macros in the SIL input format. They should
 	-- probably be refactored using Lua, but to get the party started with SILE
 	-- v0.13 package support we'll just process them here.
-	SILE.processFile(script_path() .. "macros.sil")
+	SILE.processFile(self.basedir .. "macros.sil")
 end
 
 return package
